6d333a5a8d07c8e8ff75e8038af0e452bc7e10fb
bo$$ <rasitsahbutdinov915455@gmail.com>
2023-09-12 19:44:39 +0300

    New test

--- src/test/java/MyTestDemoqa22.java
+++ src/test/java/MyTestDemoqa22.java
@@ -3,15 +3,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 public class MyTestDemoqa22 {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.pageLoadStrategy = "none";
-        Configuration.holdBrowserOpen = true;
+        Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
@@ -21,16 +19,14 @@ public class MyTestDemoqa22 {
         $x("//*[@id='firstName']").setValue("Rashit");
         $x("//*[@id='lastName']").setValue("Sakhbutdinov");
         $x("//*[@id='userEmail']").setValue("rasitsahbutdinov915455@gmail.com");
-        $(byText("Male")).click();
+        $x("//div[contains(@class, 'custom-radio')][.//*[@id='gender-radio-1']]").click();
         $x("//*[@id='userNumber']").setValue("1234567890");
         $x("//*[@id='dateOfBirthInput']").click();
         $x("//option[contains(@value, '1992')]").click();
         $x("//option[@value='3']").click();
         $x("//div[contains(@class, 'react-datepicker__day--027')]").click();
-        $x("//div[contains(@class, 'value')]").setValue("che").pressEnter();
-        $(byText("Sports")).click();
-        $(byText("Reading")).click();
-        $x("//*[@id='hobbies-checkbox-3']").click();
+        $x("//*[@id='subjectsInput']").setValue("che").pressEnter();
+        $x("//div[contains(@class, 'custom-control')][.//*[@id='hobbies-checkbox-1']]").click();
         $x("//*[@id='uploadPicture']").uploadFromClasspath("lion.jpg");
         $x("//*[@id='currentAddress']").setValue("Russia");
         $x("//*[@id='stateCity-wrapper']").click();
@@ -38,5 +34,16 @@ public class MyTestDemoqa22 {
         $x("//*[@id='city']").click();
         $x("//*[@id='react-select-4-option-1']").click();
         $x("//*[@id='submit']").click();
+
+        $x("//div[@class='modal-content']").shouldHave(text("Rashit Sakhbutdinov"));
+        $x("//div[@class='modal-content']").shouldHave(text("rasitsahbutdinov915455@gmail.com"));
+        $x("//div[@class='modal-content']").shouldHave(text("Male"));
+        $x("//div[@class='modal-content']").shouldHave(text("1234567890"));
+        $x("//div[@class='modal-content']").shouldHave(text("27 April,1992"));
+        $x("//div[@class='modal-content']").shouldHave(text("Chemistry"));
+        $x("//div[@class='modal-content']").shouldHave(text("Sports"));
+        $x("//div[@class='modal-content']").shouldHave(text("lion.jpg"));
+        $x("//div[@class='modal-content']").shouldHave(text("Russia"));
+        $x("//div[@class='modal-content']").shouldHave(text("NCR Gurgaon"));
     }
 }
