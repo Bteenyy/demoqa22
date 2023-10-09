946877ffaf86a7670f64cbf205324573ced09dfb
Rashit Sakhbutdinov <rasitsahbutdinov915455@gmail.com>
2023-10-09 20:38:31 +0300

    SecondCommit

--- src/test/java/tests/LessonTen.java
+++ src/test/java/tests/LessonTen.java
@@ -1,12 +1,8 @@
 package tests;
 
-import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.selector.ByTagAndText;
-import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -16,11 +12,11 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class LessonTen extends ConfigLessonTen {
+public class LessonTen {
     @BeforeEach
     void setup() {
         open("https://ru.dotabuff.com");
@@ -50,17 +46,17 @@ public class LessonTen extends ConfigLessonTen {
 
     static Stream<Arguments> testByThreeValue() {
         return Stream.of(
-                Arguments.of("Abaddon", List.of("Mist Coil","Aphotic Shield", "Curse of Avernus","Borrowed Time"))
+                Arguments.of("Abaddon", List.of("Abaddon", "Melee", "Carry", "Durable", "Support")),
+                Arguments.of("Axe", List.of("Axe", "Melee", "Carry", "Disabler", "Durable", "Initiator"))
         );
     }
+
     @MethodSource
     @ParameterizedTest
     void testByThreeValue(String heroName, List<String> talents) {
         $(".home-search").$("#q").setValue(heroName).pressEnter();
+        sleep(10000);
         $(".inner").$(".body-item").click();
-        $("img[data-hasqtip='2']").click();
-        //$$(".hero-secondary-ability-icons r-none-mobile").filter(visible).shouldHave(CollectionCondition.texts(talents));
-        sleep(4000);
+        $$(".header-content-title").filter(visible).shouldHave(texts(talents));
     }
 }
-    //String heroName, String talentsOfHero
