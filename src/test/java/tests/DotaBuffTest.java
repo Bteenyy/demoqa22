7056b400dc0ac0b3f9f50cd63763519ef3bfacf3
Rashit Sakhbutdinov <rasitsahbutdinov915455@gmail.com>
2023-10-11 23:23:35 +0300

    SecondCommit

--- src/test/java/tests/DotaBuffTest.java
+++ src/test/java/tests/DotaBuffTest.java
@@ -19,7 +19,7 @@ public class DotaBuffTest extends ConfigDotaBuff {
     @ValueSource(strings = {"Chen", "Pudge", "Axe"})
     @ParameterizedTest(name = "В поисковой выдаче присутствует имя героя {0} для запроса {0}")
     @Tags({@Tag("UI"), @Tag("Search"), @Tag("Smoke")})
-    void testByOneValueTest(String heroName) {
+    void byOneValueTest(String heroName) {
         dotaBuffPage.openPage()
                 .setHeroName(heroName)
                 .checkResultNameHero(heroName);
@@ -32,13 +32,13 @@ public class DotaBuffTest extends ConfigDotaBuff {
     })
     @Tags({@Tag("UI"), @Tag("Type"), @Tag("Regress")})
     @ParameterizedTest(name = "В поисковой выдаче присутствует категория героя {1} под именем героя {0}")
-    void testByTwoValueTest(String heroName, String typeOfHero) {
+    void byTwoValueTest(String heroName, String typeOfHero) {
         dotaBuffPage.openPage()
                 .setHeroName(heroName)
                 .checkResultTypeHero(typeOfHero);
     }
 
-    static Stream<Arguments> testByThreeValueTest() {
+    static Stream<Arguments> byThreeValueTest() {
         return Stream.of(
                 Arguments.of("Axe", List.of("Berserker's Call", "Battle Hunger", "Counter Helix", "Culling Blade")),
                 Arguments.of("Chen", List.of("Penitence", "Holy persuasion", "Divine favor", "Hand of god")),
@@ -49,7 +49,7 @@ public class DotaBuffTest extends ConfigDotaBuff {
     @Tags({@Tag("UI"), @Tag("Abilities"), @Tag("Regress")})
     @MethodSource
     @ParameterizedTest(name = "Во вкладе способности героя {0} присутствуют способности {1}")
-    void testByThreeValueTest(String heroName, List<String> talents) {
+    void byThreeValueTest(String heroName, List<String> talents) {
         dotaBuffPage.openPage()
                 .setHeroName(heroName)
                 .clickHeroName()
